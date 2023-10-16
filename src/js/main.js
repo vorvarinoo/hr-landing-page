1a5e55ad1343a312d1ea860da7395392dc01757f
vorvarinoo <n.styrman@yandex.ru>
2023-10-16 13:36:44 +0300

    Поправил шапку и меню в мобильное и десктопной версиях сайта

--- src/js/main.js
+++ src/js/main.js
@@ -11,8 +11,33 @@ const burger = new JustBurger( {
   menuId: 'menuList',
   isOpen: ( burger ) => {
     document.querySelector( '#overlay' ).classList.add( 'open' );
+    document.querySelector( 'html' ).classList.add( 'is-open-menu' );
   },
   isClose: ( burger ) => {
     document.querySelector( '#overlay' ).classList.remove( 'open' );
+    document.querySelector( 'html' ).classList.remove( 'is-open-menu' );
   }
 } );
+
+
+
+
+
+const header = document.querySelector( ".header__mobile-top " );
+
+let lastScrollPosition = 0;
+
+window.addEventListener( "scroll", () => {
+
+  let currentScrollPosition = window.pageYOffset;
+
+  if ( currentScrollPosition - lastScrollPosition > 0 ) {
+
+    header.classList.add( "hide" );
+  } else {
+
+    header.classList.remove( "hide" );
+  }
+
+  lastScrollPosition = currentScrollPosition;
+} )
